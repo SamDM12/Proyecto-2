1f233fd45aeb2067539fc603f6d63ca8ced9c1eb
sam <samiandresduma@gmail.com>
2024-11-02 21:37:17 -0600

    Validaciones
    Valida que no se inserte doble ni que se inserte con una persona inexistente

--- addCompetitor.sql
+++ addCompetitor.sql
@@ -4,17 +4,44 @@ USE ParalympicGames;
 
 delimiter $$
 CREATE PROCEDURE addCompetitor(IN newIdentificationNumber INT, IN newClasificationScore INT, IN newTeamName VARCHAR(15))
-	BEGIN 
-		DECLARE personID, teamID INT;
-        SELECT ID_Person INTO personID
-        FROM PERSON
-        WHERE IDENTIFICATIONNUMBER = newIdentificationNumber;
-        
-        SELECT ID_Team INTO teamID
-        FROM TEAM
-        WHERE TEAMNAME = newTeamName;
-                
-        INSERT INTO COMPETITOR (CLASIFICATIONSCORE, ID_PERSON, ID_TEAM)
-        VALUES (newClasificationScore, personID, teamID);
-    END $$ 
+BEGIN
+    DECLARE personID, teamID, existingCompetitorID INT;
+
+    -- Obtener el ID de la persona
+    SELECT ID_Person INTO personID
+    FROM PERSON
+    WHERE IDENTIFICATIONNUMBER = newIdentificationNumber;
+
+    -- Validar si se encontró la persona
+    IF personID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se encontró ninguna persona con el número de identificación especificado.';
+    END IF;
+
+    -- Obtener el ID del equipo
+    SELECT ID_Team INTO teamID
+    FROM TEAM
+    WHERE TEAMNAME = newTeamName;
+
+    -- Validar si se encontró el equipo
+    IF teamID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se encontró ningún equipo con el nombre especificado.';
+    END IF;
+
+    -- Verificar si ya existe un competidor con el mismo ID_PERSON y ID_TEAM
+    SELECT ID_COMPETITOR INTO existingCompetitorID
+    FROM COMPETITOR
+    WHERE ID_PERSON = personID AND ID_TEAM = teamID;
+
+    -- Si existe un registro, lanzar un error
+    IF existingCompetitorID IS NOT NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: El competidor ya existe en el equipo especificado.';
+    END IF;
+
+    -- Insertar el nuevo competidor si no existe duplicado
+    INSERT INTO COMPETITOR (CLASIFICATIONSCORE, ID_PERSON, ID_TEAM)
+    VALUES (newClasificationScore, personID, teamID);
+END $$
 delimiter ;
--- addPerson.sql
+++ addPerson.sql
@@ -1,21 +1,73 @@
 USE ParalympicGames;
+-- DROP PROCEDURE addPerson;
+
 delimiter $$
-CREATE PROCEDURE addPerson(IN newIdentificationNumber INT, IN newFirstName VARCHAR(15), IN newSecondName VARCHAR(15), IN newFirstLastName VARCHAR(15), IN newSecondLastName VARCHAR(15), IN newBirthDate DATE, IN countryName VARCHAR(15), IN genderName VARCHAR(15), IN identificationName VARCHAR (15))
-	BEGIN 
-		DECLARE countryID, genderTypeID, identificationTypeID INT;
-        SELECT ID_COUNTRY INTO countryID
-        FROM COUNTRY
-        WHERE COUNTRY_NAME = countryName;
-        
-        SELECT ID_GENDERTYPE INTO genderTypeID
-        FROM GENDERTYPE
-        WHERE GENDERTYPE = genderName;
-        
-        SELECT ID_IDENTIFICATIONTYPE INTO identificationTypeID
-        FROM IDENTIFICATIONTYPE
-        WHERE IDENTIFICATIONTYPE = identificationName;
-                
-        INSERT INTO PERSON (IDENTIFICATIONNUMBER, FIRSTNAME, SECONDNAME, FIRSTLASTNAME, SECONDLASTNAME, BIRTHDATE, ID_COUNTRY, ID_GENDERTYPE, ID_IDENTIFICATIONTYPE)
-        VALUES (newIdentificationNumber, newFirstName, newSecondName, newFirstLastName, newSecondLastName, newBirthDate, countryID, genderTypeID, identificationTypeID);
-    END $$ 
+CREATE PROCEDURE addPerson(
+    IN newIdentificationNumber INT, 
+    IN newFirstName VARCHAR(15), 
+    IN newSecondName VARCHAR(15), 
+    IN newFirstLastName VARCHAR(15), 
+    IN newSecondLastName VARCHAR(15), 
+    IN newBirthDate DATE, 
+    IN countryName VARCHAR(15), 
+    IN genderName VARCHAR(15), 
+    IN identificationName VARCHAR(15)
+)
+BEGIN 
+    DECLARE countryID, genderTypeID, identificationTypeID, existingPersonID INT;
+
+    -- Obtener el ID del país
+    SELECT ID_COUNTRY INTO countryID
+    FROM COUNTRY
+    WHERE COUNTRY_NAME = countryName;
+
+    -- Validar si se encontró el país
+    IF countryID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se encontró ningún país con el nombre especificado.';
+    END IF;
+
+    -- Obtener el ID del tipo de género
+    SELECT ID_GENDERTYPE INTO genderTypeID
+    FROM GENDERTYPE
+    WHERE GENDERTYPE = genderName;
+
+    -- Validar si se encontró el tipo de género
+    IF genderTypeID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se encontró ningún tipo de género con el nombre especificado.';
+    END IF;
+
+    -- Obtener el ID del tipo de identificación
+    SELECT ID_IDENTIFICATIONTYPE INTO identificationTypeID
+    FROM IDENTIFICATIONTYPE
+    WHERE IDENTIFICATIONTYPE = identificationName;
+
+    -- Validar si se encontró el tipo de identificación
+    IF identificationTypeID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se encontró ningún tipo de identificación con el nombre especificado.';
+    END IF;
+
+    -- Verificar si ya existe una persona con el mismo número de identificación
+    SELECT ID_PERSON INTO existingPersonID
+    FROM PERSON
+    WHERE IDENTIFICATIONNUMBER = newIdentificationNumber;
+
+    -- Si existe un registro, lanzar un error
+    IF existingPersonID IS NOT NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Ya existe una persona con el número de identificación especificado.';
+    END IF;
+
+    -- Insertar la nueva persona si no existe duplicado
+    INSERT INTO PERSON (
+        IDENTIFICATIONNUMBER, FIRSTNAME, SECONDNAME, FIRSTLASTNAME, SECONDLASTNAME, BIRTHDATE, 
+        ID_COUNTRY, ID_GENDERTYPE, ID_IDENTIFICATIONTYPE
+    )
+    VALUES (
+        newIdentificationNumber, newFirstName, newSecondName, newFirstLastName, 
+        newSecondLastName, newBirthDate, countryID, genderTypeID, identificationTypeID
+    );
+END $$
 delimiter ;
