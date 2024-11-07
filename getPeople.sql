64adae851b258b040c3b8cf37f54df6d63c82879
sam <samiandresduma@gmail.com>
2024-11-06 23:48:40 -0600

    getPeople updated

--- getPeople.sql
+++ getPeople.sql
@@ -1,26 +1,26 @@
 USE ParalympicGames;
-
-
  -- DROP PROCEDURE getPeople;
- 
+
 DELIMITER $$
 CREATE PROCEDURE getPeople(
     IN personFirstName VARCHAR(15),
     IN personSecondName VARCHAR(15),
     IN personFirstLastName VARCHAR(15),
     IN personSecondLastName VARCHAR(15),
-    IN teamID INT
+    IN countryID INT,            -- Nuevo parámetro para el ID de país
+    IN paralympicID INT           -- Parámetro para el ID de olimpiada
 )
 BEGIN
-    SELECT p.* 
-	FROM Person p
-	LEFT JOIN Competitor c ON p.ID_PERSON = c.ID_PERSON
-	LEFT JOIN Team t ON c.ID_TEAM = t.ID_Team
-	WHERE (personFirstName IS NULL OR p.FIRSTNAME = personFirstName)
-	  AND (personSecondName IS NULL OR p.SECONDNAME = personSecondName)
-	  AND (personFirstLastName IS NULL OR p.FIRSTLASTNAME = personFirstLastName)
-	  AND (personSecondLastName IS NULL OR p.SECONDLASTNAME = personSecondLastName)
-	  AND (teamID IS NULL OR c.ID_TEAM = teamID);
-
+    SELECT p.*, co.country_name  -- Seleccionamos el país de la persona en lugar del equipo
+    FROM Person p
+    LEFT JOIN Country co ON p.ID_COUNTRY = co.ID_Country
+    LEFT JOIN CountryXParalympic cxp ON co.ID_Country = cxp.ID_COUNTRY
+    LEFT JOIN Paralympic par ON cxp.ID_PARALYMPIC = par.ID_PARALYMPIC
+    WHERE (personFirstName IS NULL OR p.FIRSTNAME = personFirstName)
+      AND (personSecondName IS NULL OR p.SECONDNAME = personSecondName)
+      AND (personFirstLastName IS NULL OR p.FIRSTLASTNAME = personFirstLastName)
+      AND (personSecondLastName IS NULL OR p.SECONDLASTNAME = personSecondLastName)
+      AND (countryID IS NULL OR co.ID_Country = countryID)           -- Filtro para país
+      AND (paralympicID IS NULL OR par.ID_PARALYMPIC = paralympicID); -- Filtro para olimpiada
 END $$
 DELIMITER ;
