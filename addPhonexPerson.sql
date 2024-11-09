04fc58218fbcbbda44c272c0f4b9eea7daceeb76
Chedrick Daniel Uzaga López <chedrickda014@estudiantec.cr>
2024-11-08 18:41:50 -0600

    DROP PROCEDIMIENTOS
    Tuve que dropear los procedimientos porque tenía algo mal, si no lo han corrido ignoren los drops y corran los procedimientos en el orden que mencioné anteriormente

--- addPhonexPerson.sql
+++ addPhonexPerson.sql
@@ -1,4 +1,7 @@
 USE ParalympicGames;
+DROP PROCEDURE addPhonexPerson;
+DROP PROCEDURE addPhone;
+
 delimiter $$
 CREATE PROCEDURE addPhonexPerson(IN identificationNumber INT, IN phoneType VARCHAR(50), IN phoneNumber INT)
 BEGIN
@@ -6,10 +9,13 @@ BEGIN
 	call addPhone(phoneType, phoneNumber);
     SELECT ID_PHONE INTO idPhone
     FROM PHONE 
-    WHERE PHONENUMBER = phoneNumber;
+    WHERE PHONENUMBER = phoneNumber
+    LIMIT 1;
+    
     SELECT ID_PERSON INTO idPerson
     FROM person
-    WHERE IDENTIFICATIONNUMBER = identificationNumber;
+    WHERE IDENTIFICATIONNUMBER = identificationNumber
+    LIMIT 1;
     
     INSERT INTO PERSONXPHONE(ID_PERSON, ID_PHONE) VALUES(idPhone, idPerson);
 END $$
@@ -22,7 +28,8 @@ BEGIN
 	declare idPhoneType INT;
     SELECT ID_PHONETYPE INTO idPhoneType
     FROM PHONETYPE
-    WHERE PHONETYPE = phoneType;
+    WHERE PHONETYPE = phoneType
+    LIMIT 1;
     INSERT INTO PHONE(PHONENUMBER, ID_PHONETYPE) VALUES (phoneNumber, idPhoneType);
 END $$
 
