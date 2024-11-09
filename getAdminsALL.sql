2e8acc447fbb36b9169e217bca5597954ff43145
sam <samiandresduma@gmail.com>
2024-11-09 16:18:39 -0600

    correcciones getAdmins y getCoaches

--- getAdminsALL.sql
+++ getAdminsALL.sql
@@ -2,9 +2,23 @@ USE ParalympicGames;
 -- DROP PROCEDURE getAdmins;
 DELIMITER //
 
-CREATE PROCEDURE getAdmins()
+CREATE PROCEDURE getAdmins(IN admin_first_name VARCHAR(15))
 BEGIN
-    SELECT * FROM administrator;
+    SELECT 
+        Administrator.ID_ADMIN,
+        Administrator.ID_PERSON,
+        Administrator.USERNAME,
+        Person.FirstName,
+        Person.SecondName,
+        Person.FirstLastName,
+        Person.SecondLastName
+    FROM 
+        Administrator
+    LEFT JOIN 
+        Person ON Administrator.ID_PERSON = Person.ID_PERSON
+    WHERE 
+        (admin_first_name IS NULL OR Person.FirstName = admin_first_name);
 END //
 
 DELIMITER ;
+;
--- getCoachesALL.sql
+++ getCoachesALL.sql
@@ -1,11 +1,25 @@
 USE ParalympicGames;
 -- DROP PROCEDURE getCoaches;
-DELIMITER //
 
-CREATE PROCEDURE getCoaches()
+DELIMITER $$
+CREATE PROCEDURE getCoaches(
+    IN countryID INT,
+    IN paralympicID INT
+)
 BEGIN
-    SELECT c.ID_COACH, p.FIRSTNAME, p.FIRSTLASTNAME FROM coach c
-    LEFT JOIN Person p ON c.ID_PERSON = p.ID_Person;
-END //
-
+		IF countryID IS NULL AND paralympicID IS NULL THEN
+        -- Si ambos parámetros son NULL, devolver solo países únicos sin ID_Picture
+        SELECT DISTINCT *
+        FROM Coach c;
+     ELSE   
+        SELECT DISTINCT c.ID_COACH, p.FIRSTNAME, p.FIRSTLASTNAME, co.COUNTRY_NAME, par.PARALYMPICS_YEAR
+        FROM Coach c
+        LEFT JOIN Person p ON c.ID_Person = p.ID_Person
+        LEFT JOIN Country co ON p.ID_Country = co.ID_Country
+        LEFT JOIN CountryXParalympic cxp ON co.ID_Country = cxp.ID_COUNTRY
+        LEFT JOIN Paralympic par ON cxp.ID_PARALYMPIC = par.ID_PARALYMPIC
+        WHERE (countryID IS NULL OR co.ID_Country = countryID)
+          AND (paralympicID IS NULL OR par.ID_PARALYMPIC = paralympicID);
+	END IF;
+END $$
 DELIMITER ;
