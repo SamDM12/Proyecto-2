53d4ad4096d8f03cf16c0d40f5f18dbface33911
Chedrick Daniel Uzaga López <chedrickda014@estudiantec.cr>
2024-11-08 23:19:15 -0600

    Update de GetAdministrator
    Hice un update para obtener los administradores, para poder hacer filtros en la GUI

--- getAdministrator.sql
+++ getAdministrator.sql
@@ -1,10 +1,20 @@
 USE ParalympicGames;
-
+DROP PROCEDURE getAdministrators;
 delimiter $$
-CREATE PROCEDURE getAdministrators()
+
+CREATE PROCEDURE getAdministrators(
+    IN identification INT,
+    IN name1 VARCHAR(50),
+    IN lastname VARCHAR(50)
+)
 BEGIN
-	SELECT p.ID_PERSON, p.IDENTIFICATIONNUMBER, p.FIRSTNAME, p.FIRSTLASTNAME
+    SELECT p.ID_PERSON, p.IDENTIFICATIONNUMBER, p.FIRSTNAME, p.FIRSTLASTNAME
     FROM PERSON p
-    JOIN ADMINISTRATOR a ON p.ID_PERSON = a.ID_PERSON;
+    JOIN ADMINISTRATOR a ON p.ID_PERSON = a.ID_PERSON
+    WHERE
+        (identification IS NULL OR p.IDENTIFICATIONNUMBER = identification)
+        AND (name1 IS NULL OR p.FIRSTNAME = name1)
+        AND (lastname IS NULL OR p.FIRSTLASTNAME = lastname);
 END $$
+
 delimiter ;
